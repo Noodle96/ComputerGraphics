63961c5f92975db1ca0f74eb5d931be95b2b9e6d
Noodle96 <jorgealfredomarino2@gmail.com>
2023-05-08 15:09:24 -0500

    Change color

--- helloWindow/hello_window.cpp
+++ helloWindow/hello_window.cpp
@@ -38,7 +38,9 @@ int main(){
 
         // render
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        // 5, 250, 180
+        // glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(5.0/255,250.0/255,180.0/255,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
