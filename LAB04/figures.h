c48c2090cb5a597ccac48f79f1d033d0d0cf6f00
Noodle96 <jorgealfredomarino2@gmail.com>
2023-06-11 17:19:41 -0500

    Add function to draw a circle

--- LAB04/figures.h
+++ LAB04/figures.h
@@ -1,6 +1,8 @@
 #ifndef FIGURES_H
 #define FIGURES_H
 
+ #include<vector> 
+
 
 float* getFirstTriangle(int &SIZEOF_L){
 	float a = 0.1;
@@ -14,6 +16,32 @@ float* getFirstTriangle(int &SIZEOF_L){
     };
 	SIZEOF_L = sizeof(firstTriangle);
 	return firstTriangle;
+}
+
+void buildCircle(float radius, int vCount,std::vector<glm::vec3> &vertices, std::vector<unsigned int> &indices){
+    // std::vector<glm::vec3> vertices;
+    // std::vector<unsigned int> indices;
+
+    float angle = 360.0f / vCount;
+    int triangleCount = vCount - 2;
+
+    std::vector<glm::vec3> temp;
+    // positions
+    for (int i = 0; i < vCount; i++){
+        float currentAngle = angle * i;
+        float x = radius * cos(glm::radians(currentAngle));
+        float y = radius * sin(glm::radians(currentAngle));
+        float z = 0.0f;
+
+        vertices.push_back(glm::vec3(x, y, z));
+    }
+
+    // push indexes of each triangle points
+    for (int i = 0; i < triangleCount; i++){
+        indices.push_back(0);
+        indices.push_back(i + 1);
+        indices.push_back(i + 2);
+    }
 }
 
 #endif // !FIGURES_H
