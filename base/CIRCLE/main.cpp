f5a28cc94a783f8266155c760c9818e5cd4b89ff
Noodle96 <jorgealfredomarino2@gmail.com>
2023-06-11 17:08:33 -0500

    Create a circle

---
+++ base/CIRCLE/main.cpp
@@ -0,0 +1,302 @@
+
+#include "utilities.h"
+#include "shader.h"
+#include "figures.h"
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main(){
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Laboratorio 03", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // build and compile our shader program
+    // you can name your shader files however you like
+
+
+
+    Shader ourShader("1.0.shader.vs", "1.0.shader.fs");
+    // Shader ourShader2("2.0.shader.vs", "2.0.shader.fs");
+    // Shader ourShader3("3.0.shader.vs", "3.0.shader.fs");
+
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    int SIZEOF_L_FIRST_TRIANGLE;
+    // int SIZEOF_L_SECOND_TRIANGLE;
+    // int SIZEOF_L_THIRD_TRIANGLE;
+    float* firstTriangle = getFirstTriangle(SIZEOF_L_FIRST_TRIANGLE);
+    // float *secondTriangle = getSecondTriangle(SIZEOF_L_SECOND_TRIANGLE);
+    // float *thirdTriangle = getThirdTriangle(SIZEOF_L_THIRD_TRIANGLE);
+
+    std::vector<glm::vec3> verticesCirculo;
+    std::vector<unsigned int> indicesCirculo;
+    buildCircle(.7, 128, verticesCirculo, indicesCirculo);
+
+    
+    // unsigned int VBOs[3], VAOs[3];
+    // glGenVertexArrays(3, VAOs); // we can also generate multiple VAOs or buffers at the same time
+    // glGenBuffers(3, VBOs);
+    unsigned int VBO, VAO;
+	unsigned int elementBuffer;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    // element buffer object
+    glGenBuffers(1, &elementBuffer);
+
+    // //first Triangle
+    // glBindVertexArray(VAOs[0]);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+
+
+    // glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    // glBufferData(GL_ARRAY_BUFFER, SIZEOF_L_FIRST_TRIANGLE, firstTriangle, GL_STATIC_DRAW);
+
+    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // glBufferData(GL_ARRAY_BUFFER, SIZEOF_L_FIRST_TRIANGLE, firstTriangle, GL_STATIC_DRAW);
+    // std::cout << SIZEOF_L_FIRST_TRIANGLE << std::endl;
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * verticesCirculo.size(), &verticesCirculo[0], GL_STATIC_DRAW);
+    std::cout << SIZEOF_L_FIRST_TRIANGLE << std::endl;
+
+
+    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);	// Vertex attributes stay the same
+    // glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    // glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indicesCirculo.size(), &indicesCirculo[0], GL_STATIC_DRAW); // asd
+
+
+
+
+    // //second triangle
+    // glBindVertexArray(VAOs[1]);	// note that we bind to a different VAO now
+    // glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);	// and a different VBO
+    // glBufferData(GL_ARRAY_BUFFER, SIZEOF_L_SECOND_TRIANGLE, secondTriangle, GL_STATIC_DRAW);
+    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0); // because the vertex data is tightly packed we can also specify 0 as the vertex attribute's stride to let OpenGL figure it out
+    // glEnableVertexAttribArray(0);
+
+    // //third triangle
+    // glBindVertexArray(VAOs[2]);	// note that we bind to a different VAO now
+    // glBindBuffer(GL_ARRAY_BUFFER, VBOs[2]);	// and a different VBO
+    // glBufferData(GL_ARRAY_BUFFER, SIZEOF_L_THIRD_TRIANGLE, thirdTriangle, GL_STATIC_DRAW);
+    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0); // because the vertex data is tightly packed we can also specify 0 as the vertex attribute's stride to let OpenGL figure it out
+    // glEnableVertexAttribArray(0);
+
+    // render loop
+    // -----------
+    // double scaleFactor = 0.5;
+    // bool subida = true;
+    // int k = 0;
+
+    // float transationFactor = 0.1;
+    // bool topeDerecha = true;
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // be sure to activate the shader before any calls to glUniform
+        /*
+        glUseProgram(shaderProgram);
+        */
+
+        // create transformations
+
+
+
+        // glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        // glm::mat4 transform2 = glm::mat4(1.0f);
+        // glm::mat4 transform3 = glm::mat4(1.0f);
+
+
+        // glm::vec3 test = glm::vec3(0.0f, 0.0f, 1.0f);
+        // std::cout << glm::to_string(test) << std::endl;
+        // std::cout << glm::to_string(transform) << std::endl;
+        //transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+
+        //EXAMPLE WITH SCALE
+        // glm::vec4 vec(-8.0f, 1.0f, 5.0f, 1.0f);
+        // glm::mat4 trans = glm::mat4(1.0f);
+        // trans = glm::scale(trans, glm::vec3(0.1f, 0.3f, 0.5f));
+        // std::cout << glm::to_string(trans) << std::endl;
+        // vec = trans * vec;
+        // std::cout << "test: "<< vec.x << " " << vec.y << " " << vec.z << std::endl;
+
+
+        //EXAMPLE WITH TRANSLATE
+        // glm::vec4 vec(-8.0f, 1.0f, 5.0f, 1.0f);
+        // glm::mat4 trans = glm::mat4(1.0f);
+        // trans = glm::translate(trans, glm::vec3(0.1f, 0.3f, 0.5f));
+        // std::cout << glm::to_string(trans) << std::endl;
+        // vec = trans * vec;
+        // std::cout << "test: "<< vec.x << " " << vec.y << " " << vec.z << std::endl;
+
+
+        //EXAMPLE WITH ROTATE
+        // glm::vec4 vec(5.0f, 5.0f, 0.0f, 0.0f);
+        // glm::mat4 trans = glm::mat4(1.0f);
+        // trans = glm::rotate(trans, glm::radians(30.0f), glm::vec3(1.0, 0.0, 0.0));
+        // std::cout << glm::to_string(trans) << std::endl;
+        // vec = trans * vec;
+        // std::cout << "test: "<< vec.x << " " << vec.y << " " << vec.z << std::endl;
+
+        //LOGICA DE LA SCALA
+        // if(subida){
+        //     scaleFactor += 0.005;
+        //     if(scaleFactor >= 1.25) subida = false;
+        // }else{
+        //     scaleFactor-= 0.005;
+        //     if(scaleFactor <= 0.5) subida = true;
+        // }
+
+
+        //LOGICA DELA TRANSLACION
+        // if(topeDerecha){
+        //     transationFactor += 0.005;
+        //     if(transationFactor >= 1.2) topeDerecha=false;
+        // }else{
+        //     transationFactor -= 0.005;
+        //     if(transationFactor <= -0.2) topeDerecha = true;
+        // }
+        // std::cout <<traslationFactor << std::endl;
+
+        // if(k <=200){
+        //     std::cout << glm::to_string(transform) << std::endl;
+        //     k++;    
+        // }
+
+        //FIRST TRIANGLE
+        // ourShader.use();
+        // glBindVertexArray(VAO);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glBindVertexArray(VAOs[0]);
+        // update shader uniform
+        //double  timeValue = glfwGetTime();
+        //std::cout << "timeValue: " << timeValue << std::endl;
+        // float greenValue = static_cast<float>(sin(timeValue) / 2.0 + 0.5);
+        //float greenValue = static_cast<float>(fabs(sin(1.2*timeValue)));
+        //std::cout << "greenValue: " << greenValue << std::endl;
+
+
+        //int vertexColorLocation = glGetUniformLocation(ourShader.ID, "ourColor");
+        // ourShader.changeOurColor(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        // // glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+        // transform = glm::scale(transform, glm::vec3(scaleFactor, scaleFactor, scaleFactor));
+        // // std::cout << glm::to_string(transform) << std::endl;
+        // unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        // glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+        // // render the triangle
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+
+
+        //SECOND TRIANGLE
+        // transform2 = glm::scale(transform2, glm::vec3(1.75-scaleFactor, 1.75-scaleFactor, 1.75-scaleFactor));
+        // ourShader2.use();
+        // glBindVertexArray(VAOs[1]);
+        // float redValue = static_cast<float>(fabs(sin(1.2*timeValue)));
+        // vertexColorLocation = glGetUniformLocation(ourShader2.ID, "ourColor");
+        // ourShader2.changeOurColor(vertexColorLocation, (1.0-redValue), 0.0f, 0.0f, 1.0f);
+
+
+        // transform2 = glm::translate(transform2, glm::vec3(transationFactor, 0, 0));
+        // // std::cout << glm::to_string(transform2) << std::endl;
+        // unsigned int transformLoc2 = glGetUniformLocation(ourShader2.ID, "transform2");
+        // glUniformMatrix4fv(transformLoc2, 1, GL_FALSE, glm::value_ptr(transform2));
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+
+        // //THIRD TRIANGLE
+        // ourShader3.use();
+        // glBindVertexArray(VAOs[2]);
+
+        // transform3 = glm::translate(transform3, glm::vec3(0.4f, 0.2f, 0.0f));
+        // transform3 = glm::rotate(transform3, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        // transform3 = glm::translate(transform3, glm::vec3(-0.4f, -0.2f, 0.0f));
+        // // transform3 = glm::translate(transform3, glm::vec3(-0.4f, -0.2f, 0));
+
+
+        // // std::cout << (float)glfwGetTime() << std::endl;
+        // // transform2 = glm::translate(transform2, glm::vec3(1.0f, 1.0f, 0.0f));
+        // //transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        // unsigned int transformLoc3 = glGetUniformLocation(ourShader3.ID, "transform3");
+        // glUniformMatrix4fv(transformLoc3, 1, GL_FALSE, glm::value_ptr(transform3));
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // RENDER CIRCLE
+        ourShader.use();
+        glBindVertexArray(VAO);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, indicesCirculo.size(), GL_UNSIGNED_INT, 0);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    /*
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    */
+    // glDeleteVertexArrays(3, VAOs);
+    // glDeleteBuffers(3, VBOs);
+
+    //glDeleteProgram(shaderProgram);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
---
+++ base/CIRCLE/main2.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <cmath>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/vec3.hpp> //
+#include <glm/glm.hpp>  //
+// #include "shaderprog.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <vector>
+#include "shader.h"
+using namespace std;
+
+std::vector<glm::vec3> vertices;
+std::vector<unsigned int> indices;
+
+
+void buildCircle(float radius, int vCount)
+{
+    float angle = 360.0f / vCount;
+
+    int triangleCount = vCount - 2;
+
+    std::vector<glm::vec3> temp;
+    // positions
+    for (int i = 0; i < vCount; i++)
+    {
+        float currentAngle = angle * i;
+        float x = radius * cos(glm::radians(currentAngle));
+        float y = radius * sin(glm::radians(currentAngle));
+        float z = 0.0f;
+
+        vertices.push_back(glm::vec3(x, y, z));
+    }
+
+    // push indexes of each triangle points
+    for (int i = 0; i < triangleCount; i++)
+    {
+        indices.push_back(0);
+        indices.push_back(i + 1);
+        indices.push_back(i + 2);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    // initialize GLFW
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    // create a window pointer
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello Circle", NULL, NULL);
+    // error check
+    if (window == NULL)
+    {
+        cout << "Error. I could not create a window at all!" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    buildCircle(.7, 8);
+	for(int e = 0 ; e < vertices.size(); e++){
+		std::cout << glm::to_string(vertices[e]) << endl;
+	}
+
+    Shader ourShader("1.0.shader.vs", "1.0.shader.fs");
+
+    // attachment of shaders to program object
+    // myProgram.attach("./shaders/vs.glsl", GL_VERTEX_SHADER);
+    // myProgram.attach("./shaders/fs.glsl", GL_FRAGMENT_SHADER);
+    // myProgram.link();
+
+    // buffer
+	// object id variables
+	unsigned int vertexBuffer;
+	unsigned int vertexArray;
+	unsigned int elementBuffer;
+    glGenVertexArrays(1, &vertexArray);
+    glGenBuffers(1, &vertexBuffer);
+
+    // element buffer object
+    glGenBuffers(1, &elementBuffer);
+
+    glBindVertexArray(vertexArray);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+    // copying
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * vertices.size(), &vertices[0], GL_STATIC_DRAW); // asd
+	// with 8 rectangles
+	// cout << sizeof(glm::vec3) << endl; // 12
+	// cout << vertices.size() << endl; // 8
+	// cout << sizeof(glm::vec3) * vertices.size() << endl; // 96
+	cout << "printing ... " << endl;
+	cout << &vertices[0] << endl;
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), &indices[0], GL_STATIC_DRAW); // asd
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.0f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ourShader.use();
+
+        glBindVertexArray(vertexArray);
+
+        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0); // asd
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
