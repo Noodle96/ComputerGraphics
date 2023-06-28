d37236e07ef918f4bcaeb3b453c91111742c932a
Noodle96 <jorgealfredomarino2@gmail.com>
2023-06-28 10:07:08 -0500

    update texturas without indices

--- base/texturas/texturas_main.cpp
+++ base/texturas/texturas_main.cpp
@@ -278,29 +278,39 @@ int main()
     //     -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
     //     -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
     // };
-    float vertices[] = {
-        // positions          // texture coords
-         0.5f,  0.5f, 0.0f,    1.0f, 1.0f, // top right
-         0.5f, -0.5f, 0.0f,     1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f,      0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,     0.0f, 1.0f  // top left 
+    // float vertices[] = {
+    //     // positions          // texture coords
+    //      0.5f,  0.5f, 0.0f,    1.0f, 1.0f, // top right    // 0
+    //      0.5f, -0.5f, 0.0f,     1.0f, 0.0f, // bottom right // 1
+    //      -0.5f,  0.5f, 0.0f,     0.0f, 1.0f,  // top left  // 3
+    //       0.5f, -0.5f, 0.0f,     1.0f, 0.0f, // bottom right // 1
+    //     -0.5f, -0.5f, 0.0f,      0.0f, 0.0f, // bottom left // 2
+    //     -0.5f,  0.5f, 0.0f,     0.0f, 1.0f,  // top left  // 3
+    //     0.5f,  0.5f, 0.0f,    1.0f, 1.0f, // top right    // 0
+    // };
+    float a = 0.1;
+    float vertices [] = {
+        -8*a, 1*a, 0.0f,       1.0f, 1.0f, //botton left
+        -2*a, 1*a, 0.0f,    1.0f, 0.0f,  //bottom right
+        -5*a, 7*a, 0.0f,  0.0f, 1.0f, //top
     };
-    unsigned int indices[] = {  
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
-    };
+    
+    // unsigned int indices[] = {  
+    //     0, 1, 3, // first triangle
+    //     1, 2, 3  // second triangle
+    // };
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+    // glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // // position attribute
     // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -371,7 +381,8 @@ int main()
         // render container
         ourShader.use();
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
