9f087bc04bbaee8895f4c5076597a3e7a06c3975
Noodle96 <jorgealfredomarino2@gmail.com>
2023-06-28 10:07:24 -0500

    Init Lab05

---
+++ LAB05/libraries.h
@@ -0,0 +1,15 @@
+#ifndef LIBRARIES_H
+#define LIBRARIES_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <cmath>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#endif // !LIBRARIES_H
